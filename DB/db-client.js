df23d2e5e1df7fe7a523c3756daa580915553e6c
ragunath <raguyuvi27@gmail.com>
2023-11-07 01:12:32 +0530

    client

--- DB/db-client.js
+++ DB/db-client.js
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb";
 
-const url = "mongodb://0.0.0.0:27017/";
+// const url = "mongodb://0.0.0.0:27017/";
+
+const url = process.env.CONNECTION_URL;
 
 const client = new MongoClient(url);
 
